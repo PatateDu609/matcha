d9f2ab56586846573adfff763d1566139814fd23
Ghali Boucetta <gboucett@student.42.fr>
2023-01-12 23:54:49 +0100

    add bcrypt hash to sign-up backend

--- back/routes/setup.go
+++ back/routes/setup.go
@@ -3,9 +3,9 @@ package routes
 import (
 	"github.com/PatateDu609/matcha/utils/database"
 	"github.com/PatateDu609/matcha/utils/log"
-	"github.com/go-chi/chi/middleware"
-
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 )
 
 func Setup() (router *chi.Mux) {
@@ -15,6 +15,7 @@ func Setup() (router *chi.Mux) {
 	router.Use(log.NewRouterLogger(log.Logger))
 	router.Use(middleware.Recoverer)
 	router.Use(database.AcquireMiddleware)
+	router.Use(cors.AllowAll().Handler)
 
 	router.Group(func(r chi.Router) {
 		r.Use(middleware.AllowContentType("application/json"))

75b0822a773d6b7bff8f45e23438cd4be962c755
Ghali Boucetta <gboucett@student.42.fr>
2023-01-12 01:57:08 +0100

    add sign up view

---
+++ front/src/pages/SignUpPage.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <q-page class="row justify-center items-center" padding>
+    <div class="row col justify-center">
+      <div class="col" style="max-width: 400px">
+        <q-form class="column q-gutter-md">
+          <q-input type="text" outlined name="username" label="Username" />
+          <q-input type="password" outlined name="password" label="Password" />
+          <q-input type="text" outlined name="email" label="Email" />
+          <q-input type="text" outlined name="first-name" label="First Name" />
+          <q-input type="text" outlined name="last-name" label="Last Name" />
+
+          <q-btn color="secondary" type="submit">Sign up</q-btn>
+        </q-form>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<style scoped>
+
+</style>
--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,7 +1,12 @@
-import { route } from "quasar/wrappers";
-import { createMemoryHistory, createRouter, createWebHashHistory, createWebHistory } from "vue-router";
+import { route } from 'quasar/wrappers';
+import {
+  createMemoryHistory,
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from 'vue-router';
 
-import routes from "./routes";
+import routes from './routes';
 
 /*
  * If not building with SSR mode, you can
@@ -15,7 +20,9 @@ import routes from "./routes";
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+    : process.env.VUE_ROUTER_MODE === 'history'
+    ? createWebHistory
+    : createWebHashHistory;
 
   return createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),

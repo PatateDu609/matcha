13748373a2c9fd270a2a5802f22765dac0ba47ea
Ghali Boucetta <gboucett@student.42.fr>
2023-01-28 00:24:39 +0100

    fix delete

--- back/utils/database/delete.go
+++ back/utils/database/delete.go
@@ -6,7 +6,6 @@ import (
 )
 
 func Delete(ctx context.Context, name string, cond *Condition) error {
-
 	conn, err := GetConnFromCtx(ctx)
 	if err != nil {
 		return err
@@ -14,11 +13,11 @@ func Delete(ctx context.Context, name string, cond *Condition) error {
 
 	req := fmt.Sprintf("DELETE FROM %s WHERE %q", name, cond.String())
 
-	res, err := conn.Exec(ctx, req)
+	res, err := conn.Exec(ctx, req, cond.Values()...)
 	if err != nil {
 		return fmt.Errorf("deletion failed: %s", err)
 	}
-	if !res.Insert() {
+	if !res.Delete() {
 		return fmt.Errorf("deletion failed: unknown error")
 	}
 	return nil
